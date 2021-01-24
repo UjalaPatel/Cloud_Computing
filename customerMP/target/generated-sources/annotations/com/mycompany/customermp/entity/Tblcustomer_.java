01f2ba9220866ad12aedab6b9dbc39be60a9a7b6
UjalaPatel <ujalapatel1907@gmail.com>
2021-01-24 22:48:16 +0530

    customer_Microprofile_Application

---
+++ customerMP/target/generated-sources/annotations/com/mycompany/customermp/entity/Tblcustomer_.java
@@ -0,0 +1,18 @@
+package com.mycompany.customermp.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-01-24T14:34:38")
+@StaticMetamodel(Tblcustomer.class)
+public class Tblcustomer_ { 
+
+    public static volatile SingularAttribute<Tblcustomer, String> address;
+    public static volatile SingularAttribute<Tblcustomer, Integer> customerID;
+    public static volatile SingularAttribute<Tblcustomer, String> name;
+    public static volatile SingularAttribute<Tblcustomer, Integer> rating;
+    public static volatile SingularAttribute<Tblcustomer, String> email;
+    public static volatile SingularAttribute<Tblcustomer, String> phoneNo;
+
+}
